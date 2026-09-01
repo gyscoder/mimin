17bf6c32a07a70870486ef6f57dc14c8078c5459
gyscoder <geysomsilva.20@gmail.com>
2026-09-01 13:04:12 -0300

    Deploy para o GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      '1cc0-179-125-215-78.ngrok-free.app'
-    ]
-  }
+  base: '/mimin/',
 })
